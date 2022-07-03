7aecb3b041f50706a1d2b3cc1b0d8af84acf89c8
techieatkct <108172869+techieatkct@users.noreply.github.com>
2022-07-03 11:48:05 +0530

    Initial commit

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,20 @@
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
 const newGrid = (size) => {
     let arr = new Array(size);
 
