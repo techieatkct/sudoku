6722807ba86ba510e066d01c2b27511b6cad460d
techieatkct <108172869+techieatkct@users.noreply.github.com>
2022-07-03 11:47:34 +0530

    Initial commit

--- constant.js
+++ constant.js
@@ -1,3 +1,20 @@
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
 const CONSTANT = {
     UNASSIGNED: 0,
     GRID_SIZE: 9,
